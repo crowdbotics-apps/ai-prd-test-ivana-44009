99c9c933420c6a31bddae9bae1acabe4b75cb103
Crowdbotics <team@crowdbotics.com>
2023-10-18 16:36:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ai-prd-test-ivana-44009",
+  url: "https://ai-prd-test-ivana-44009.botics.co",
+  api: "https://ai-prd-test-ivana-44009.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
